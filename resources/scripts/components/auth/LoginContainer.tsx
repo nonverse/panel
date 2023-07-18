dcad0e62de2526915fa942317b2bfd179df29570
Isuru <isuru2003a@gmail.com>
2023-07-18 20:20:48 +1000

    Show screenblock if user is not authorized to use the panel application

--- resources/scripts/components/auth/LoginContainer.tsx
+++ resources/scripts/components/auth/LoginContainer.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import useFlash from '@/plugins/useFlash';
 import http from "@/api/http";
 import Spinner from "@/components/elements/Spinner";
+import NotFoundSvg from '@/assets/images/not_found.svg';
+import ScreenBlock from "@/components/elements/ScreenBlock";
 
 function LoginContainer() {
 
-    const { clearFlashes } = useFlash();
+    const {clearFlashes} = useFlash();
     const query = new URLSearchParams(window.location.search)
+    const [authorized, setAuthorized] = useState(true)
 
     useEffect(() => {
         clearFlashes();
@@ -25,6 +28,9 @@ function LoginContainer() {
                     if (e.response.status === 401) {
                         window.location = e.response.data.data.auth_url
                     }
+                    if (e.response.status === 403) {
+                        setAuthorized(false)
+                    }
                 })
         }
 
@@ -32,13 +38,18 @@ function LoginContainer() {
     }, []);
 
     return (
-        <div className="awaiting-oauth absolute w-screen h-screen top-0" style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-        }}>
-            <Spinner/>
-        </div>
+        <>
+            {authorized ? (
+                <div className="awaiting-oauth absolute w-screen h-screen top-0" style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}>
+                    <Spinner/>
+                </div>
+            ) : <ScreenBlock title="Forbidden" image={NotFoundSvg} message="You are not authorized to use this application"/>
+            }
+        </>
     );
 }
 
