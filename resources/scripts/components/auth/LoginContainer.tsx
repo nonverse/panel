76ea4a384c1a56410f5bb5fe6ee1218d83e7f02b
Isuru <isuru2003a@gmail.com>
2023-07-18 18:12:46 +1000

    Show spinner while application is authenticating

--- resources/scripts/components/auth/LoginContainer.tsx
+++ resources/scripts/components/auth/LoginContainer.tsx
@@ -1,16 +1,41 @@
 import { useEffect } from 'react';
 import useFlash from '@/plugins/useFlash';
+import http from "@/api/http";
+import Spinner from "@/components/elements/Spinner";
 
 function LoginContainer() {
 
     const { clearFlashes } = useFlash();
+    const query = new URLSearchParams(window.location.search)
 
     useEffect(() => {
         clearFlashes();
+
+        async function login() {
+            await http.post('/auth/login', {
+                code: query.get('code')
+            })
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(e => {
+                    if (e.response.status === 401) {
+                        window.location = e.response.data.data.auth_url
+                    }
+                })
+        }
+
+        login()
     }, []);
 
     return (
-        <div>Awaiting OAuth2 Authorization</div>
+        <div className="awaiting-oauth absolute w-screen h-screen top-0" style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+        }}>
+            <Spinner/>
+        </div>
     );
 }
 
